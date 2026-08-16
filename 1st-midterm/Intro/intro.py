f44782723448edd9f55f6c97ca6241a46343a5d9
Bruno Sánchez <90653850+BrunoSaPa@users.noreply.github.com>
2026-08-16 15:34:24 -0600

    Create intro.py

---
+++ 1st-midterm/Intro/intro.py
@@ -0,0 +1,12 @@
+var1 = 2
+var2 = 2.3
+var3 = "Ejemplo"
+var4 = 'Ejemplo'
+var5 = [1,2,3]
+var6 = ['a','b','c']
+var7 = [var5,var6]
+var8 = {'Dato1':10,'Data2':20,'Dato3':30}
+var9 = [var7, var8]
+var10 = {'Diccionario':var8,'Lista':var9}
+var11 = (1,2,3,4,5)
+
